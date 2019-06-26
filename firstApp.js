109516efac66ecef4b4ac4ffbaaf75899f7a77b9
jose <josecarlosesparza@gmail.com>
2019-06-26 12:05:15 +0100

    route * added

--- firstApp.js
+++ firstApp.js
@@ -3,11 +3,13 @@ var app = express();
 
 // "/" => "Hi There!"
 app.get("/", function(req, res){
+    console.log("Someone made a request to /");
 	res.send("Hi there!");
 });
 
 // "/bye" => "Goodbye!"
 app.get("/bye", function(req, res){
+    console.log("Someone made a request to /bye");
 	res.send("Goodbye!");
 });
 
@@ -15,6 +17,11 @@ app.get("/bye", function(req, res){
 app.get("/dog", function(req, res){
 	console.log("Someone made a request to /dog");
 	res.send("MEAOW!");
+});
+
+// "*" all routes that haven't matched before
+app.get("*", function(req, res){
+	res.send("OOPS! Nothing here!");
 });
 
 //Tell Express to listen for requests (start server)
